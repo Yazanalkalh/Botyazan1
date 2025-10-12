7caf419931e8b95105a6599c8764045ca8eae6d6
Yazanalkalh <dood20301@gmail.com>
2025-10-12 03:09:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ from bot.handlers.user.message_handler import message_forwarder_handler
 from bot.handlers.admin.main_panel import admin_handler, admin_panel_back_handler
 from bot.handlers.admin.approval_handler import channel_approval_tracker, channel_decision_handler
 from bot.handlers.admin.communication_handler import admin_reply_handler
-from bot.handlers.admin.reminders_handler import *
-from bot.handlers.admin.interface_handler import *
+# --- هنا التعديل: نستورد كل شيء من ملف التذكيرات ---
+from bot.handlers.admin.reminders_handler import * from bot.handlers.admin.interface_handler import *
 from bot.handlers.admin.subscription_handler import *
 from bot.handlers.admin.text_editor_handler import *
 
@@ -44,8 +44,11 @@ async def main():
 
         # معالجات المدير - اللوحة الرئيسية والوحدات
         admin_handler, admin_panel_back_handler,
-        reminders_menu_handler, view_all_reminders_handler, delete_reminder_handler, add_reminder_conversation_handler,
-        interface_menu_handler, change_timezone_conversation_handler,
+        
+        # --- هنا التصحيح: نستخدم الأسماء الصحيحة للمعالجات ---
+        reminders_panel_handler, view_all_reminders_handler, delete_reminder_handler, add_reminder_conv_handler, import_reminders_conv_handler,
+        
+        interface_menu_handler, change_timezone_conv_handler,
         subscription_menu_handler, view_channels_main_handler, delete_channel_main_handler, add_channel_conversation_handler,
         edit_texts_menu_handler, edit_texts_conversation_handler
     ]
